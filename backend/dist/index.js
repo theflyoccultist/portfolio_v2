62ec2644cdd702fbaf4e2efcad88cc989c4b329f
theflyoccultist <y.delahaije@gmail.com>
2024-09-05 13:27:51 +0200

    update backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -21,7 +21,7 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
 app.use(express_1.default.json());
 app.use((0, cors_1.default)({
-    origin: 'http://localhost:5173',
+    origin: 'http://rinkakuworks.com',
     methods: ['GET', 'POST'],
     credentials: true
 }));
