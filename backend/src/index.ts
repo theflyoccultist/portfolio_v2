e5ef9dcd2d9d08a983bb4ba9d61b298ded631bab
theflyoccultist <y.delahaije@gmail.com>
2024-09-09 23:14:31 +0200

    backend fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://rinkakuworks.com'],
+    origin: ['http://rinkakuworks.com/'],
     methods: ['GET', 'POST'],
     credentials: true
 }));
